e2721e7bfcfa60b3a2e5b10f6ecc0e8815f7fe69
Brisan Andrei-Sebastian <brisanandrei@gmail.com>
2024-11-09 15:21:47 +0200

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Routers, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
+import Dashboard from './pages/dashboard'
+import Courses from './pages/courses'
+import Login from './pages/auth/login'
+import Signup from './pages/auth/signup'
+
 
 const App = () =>
 (
   <>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/dashboard'element={<Dashboard/>}/>
+      <Route path='/courses'element={<Courses/>}/>
     </Routes>
   </>
 )
